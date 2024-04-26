bb59893568254b4f9cbd903a402502cb3860eb03
i3NakashimaTakumi <nakashima.takumi@i3design.co.jp>
2024-04-26 11:33:01 +0900

    change rollback description

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,13 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
-    move > 0 ? description = "Go to move #" + move : description = "Go to game start";
+    if (move === currentMove) {
+      description = "You are at move #" + move;
+    } else if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}> {description} </button>
