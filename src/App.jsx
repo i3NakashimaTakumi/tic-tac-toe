f50e53855369b1d80f978e1dfa3addfdf260d1b6
i3NakashimaTakumi <nakashima.takumi@i3design.co.jp>
2024-04-26 11:21:38 +0900

    add roll back feature. complete tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -30,33 +30,49 @@ function Board({ xIsNext, squares, onPlay }) {
     <>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value = {squares[0]} onSquareClick = {() => handleClick(0)}/>
-        <Square value = {squares[1]} onSquareClick = {() => handleClick(1)}/>
-        <Square value = {squares[2]} onSquareClick = {() => handleClick(2)}/>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
       </div>
       <div className="board-row">
-        <Square value = {squares[3]} onSquareClick = {() => handleClick(3)}/>
-        <Square value = {squares[4]} onSquareClick = {() => handleClick(4)}/>
-        <Square value = {squares[5]} onSquareClick = {() => handleClick(5)}/>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
       </div>
       <div className="board-row">
-        <Square value = {squares[6]} onSquareClick = {() => handleClick(6)}/>
-        <Square value = {squares[7]} onSquareClick = {() => handleClick(7)}/>
-        <Square value = {squares[8]} onSquareClick = {() => handleClick(8)}/>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </>
   );
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    move > 0 ? description = "Go to move #" + move : description = "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}> {description} </button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
@@ -64,7 +80,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
